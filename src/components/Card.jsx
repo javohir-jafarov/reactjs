173890871efbbf6996a2eeffd8fd2333a74025b4
Javohir <jafarjavohir2010@gmail.com>
2026-07-17 20:54:58 +0500

    hello ubuntu

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,7 @@ import profilePic from '../assets/profile.jpg'
 
 const Card = (props) => {
   return (
-    <div className='flex items-center border-black border-2 w-36'>
-        <img src={profilePic} alt="profile picture" className="profile w-[150px] w-[150px]" />
-        <div className="text">
-            <h2>Name: {props.name}</h2>
-            <p>Age: {props.age}</p>
-        </div>
-    </div>
+    
   )
 }
 
