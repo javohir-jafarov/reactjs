366aa35f5c1a2e146c3aa26e94e61001a6d0ab19
Javohir <jafarjavohir2010@gmail.com>
2026-07-18 17:19:31 +0500

    data baseni App.jsx ga utqazdim va ushani List.jsx ga ulashni urgandim. render Lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import react from 'react'
 import List from './components/List'
 
+let fruits = [{id: 1,name: 'apple', cal: 95},
+              {id: 2,name: 'banana', cal: 159},
+              {id: 3,name: 'pineapple', cal: 97},
+              {id: 4,name: 'cherry', cal: 105},
+              {id: 5,name: 'coconut', cal: 115},]
 
+let vegie = [{id: 6,name: 'carrot', cal: 55},
+              {id: 7,name: 'tomato', cal: 129},
+              {id: 8,name: 'potato', cal: 70},
+              {id: 9,name: 'cabbage', cal: 105},
+              {id: 10,name: 'onion', cal: 100},]
 
 let App = () => {
   return (
-    <>
-      <div className="cards">
-        <List />
-      </div>
+    <>  
+        <List item={fruits} category='fruits'/>
+        <List item={vegie} category='vegie'/>
     </>
   )
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,34 +1,16 @@
 import React from 'react'
 
-let fruits = [{id: 1,name: 'apple', cal: 95},
-              {id: 2,name: 'banana', cal: 159},
-              {id: 5,name: 'pineapple', cal: 97},
-              {id: 3,name: 'cherry', cal: 105},
-              {id: 4,name: 'coconut', cal: 115},]
-  
+const List = (props) => {
 
-// fruits.sort((a, b) => a.name.localeCompare(b.name)) // alphabetical
-// fruits.sort((a, b) => b.name.localeCompare(a.name)) // reverse alphabetical
-// fruits.sort((a, b) => a.cal - b.cal) // Numeric
-// fruits.sort((a, b) => b.cal - a.cal) // reverse Numeric
+  const itemList = props.item
+  let ListItems = itemList.map(item => <li key={item.id}>{item.name}:&nbsp;{item.cal}</li>)
 
 
-//const lowCalFruits = fruits.filter(fruit => fruit.cal <= 150) // 150 dan kamini filtirlebdi
-
-// const highCalFruits = fruits.filter(highCalFruit => highCalFruit.cal >= 100) // 100 dan kupini filterledi
-
-const ListedFruits = highCalFruits.map(highCalFruit => 
-
-<li key={highCalFruit.id}>
-  {highCalFruit.name}:&nbsp;<b>{highCalFruit.cal}</b>
-</li>)
-
-
-const List = () => {
   return (
-    <>
-      <ul>{ListedFruits}</ul>
-    </>
+    <div>
+      <h1 className='text-4xl'>{props.category}</h1>
+      <ol>{ListItems}</ol>
+    </div>
   )
 }
 
