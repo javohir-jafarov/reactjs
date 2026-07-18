84d2dc69f0a9491187f1831189ef776f3e37198b
Javohir <jafarjavohir2010@gmail.com>
2026-07-18 15:59:09 +0500

    sorting

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 
-let fruits = [
-  {id: 1,name: 'apple', cal: 95},
-  {id: 2,name: 'banana', cal: 159},
-  {id: 3,name: 'cherry', cal: 105},
-  {id: 4,name: 'coconut', cal: 115},
-  {id: 5,name: 'pineapple', cal: 97},
-]
+let fruits = [{id: 1,name: 'apple', cal: 95},
+              {id: 2,name: 'banana', cal: 159},
+              {id: 5,name: 'pineapple', cal: 97},
+              {id: 3,name: 'cherry', cal: 105},
+              {id: 4,name: 'coconut', cal: 115},]
+  
 
-fruits.sort()
+// fruits.sort((a, b) => a.name.localeCompare(b.name)) // alphabetical
+// fruits.sort((a, b) => b.name.localeCompare(a.name)) // reverse alphabetical
+// fruits.sort((a, b) => a.cal - b.cal) // Numeric
+fruits.sort((a, b) => b.cal - a.cal) // reverse Numeric
 
-let ListedFruits = fruits.map(fruit => <li key={fruit.id}>{fruit.name}:&nbsp;{fruit.cal}</li>)
+
+
+const ListedFruits = fruits.map(fruit => 
+
+<li key={fruit.id}>
+  {fruit.name}:&nbsp;<b>{fruit.cal}</b>
+</li>)
 
 
 const List = () => {
