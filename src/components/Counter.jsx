dca1a5f63af3a057347478c90154fb88f5c457a0
Javohir <jafarjavohir2010@gmail.com>
2026-07-23 15:43:30 +0500

    create data base for counter app

---
+++ src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const buttons = [
+    {id: 1, textContent: '-'},
+    {id: 2, textContent: 'reset'},
+    {id: 3, textContent: '+'},
+]
+
+const Counter = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Counter
