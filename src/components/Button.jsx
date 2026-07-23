57f113750a4bfde9abca22ac36192ff6259f89b8
Javohir <jafarjavohir2010@gmail.com>
2026-07-23 16:12:50 +0500

    useState done!

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Button = (props) => {
-  return <button key={props.id} className='w-1/3 h-2/1 cursor-pointer text-2xl rounded-[10px] bg-emerald-400 '>{props.innerText}</button>
+  return <button key={props.id} onClick={props.func} className='w-1/3 h-2/1 cursor-pointer text-2xl rounded-[10px] bg-emerald-400 '>{props.innerText}</button>
 }
 
 export default Button
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,38 @@
 import React, {useState} from 'react'
 import Button from './Button'
 
-const buttons = [
-    {id: 1, textContent: '-'},
-    {id: 2, textContent: 'reset'},
-    {id: 3, textContent: '+'},
-]
+
+
+
 
 const Counter = () => {
+
+    const buttons = [
+    {id: 1, textContent: '-', func: () => decrement()},
+    {id: 2, textContent: 'reset', func: () => reset()},
+    {id: 3, textContent: '+', func: () => increment()},
+]
+
+
+    let decrement = () => {
+        setNum(num - 1)
+    }
+
+    let reset = () => {
+        setNum(num = 0)
+    }
+    let increment = () => {
+        setNum(num + 1)
+    }
+
+
+
+    let [num, setNum] = useState(0)
   return (
     <div className='counterApp rounded-2xl flex flex-col items-center border w-100 mx-auto h-100 justify-around'>
-        <p className='text-5xl'>{0}</p>
+        <p className='text-5xl'>{num}</p>
         <div className="buttons px-1 gap-1 w-1/1 flex items-center justify-around">
-                    {buttons.map(item => <Button  innerText={item.textContent} id={buttons.id}/>)}
+                    {buttons.map(item => <Button  innerText={item.textContent} id={item.id} func={item.func}/>)}
         </div>
     </div>
   )
