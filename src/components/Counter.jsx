877d34a92f17f34fd8a9f379366e44ff192101b9
Javohir <jafarjavohir2010@gmail.com>
2026-07-23 10:58:43 +0500

    in

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,14 +3,14 @@ import Button from './Button'
 
 let num = 12
 
-const increase = () => {h1.props.children = num += 1}
+const increase = (number) => {h1.props.children = number += 1}
 const decrease = () => {num--}
 const reset = () => {num = 0}
 
 const buttonObj = [
     {func: decrease,id: 1, name: '-'},
     {func: reset,id: 2, name: 'reset'},
-    {func: increase,id: 3, name: '+'}
+    {func: () => increase(num),id: 3, name: '+'}
 ]
 
 let h1 = <h1>{num}</h1>
