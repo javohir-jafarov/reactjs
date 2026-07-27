09612202fab252d4976b95a425bf65c3ef875598
Javohir <jafarjavohir2010@gmail.com>
2026-07-27 17:17:07 +0500

    done with number

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,29 @@ import react, { useState } from "react";
 function App() {
 
   let [name, setName] = useState('Guest')
-
+  let [quantity, setQuantity] = useState(1)
+  let []
 
   function nameHandler(e) {
     setName(e.target.value)
+  }
+  function quantityHandler(e) {
+    setQuantity(e.target.value)
   }
   return (
     <>
       <input onChange={nameHandler} className="border" type="text" name="" id="text" />
       <p>Name: {name}</p>
       
+      {/*  */}
+
+      <input type="number" name="" id="number" className="border" onChange={quantityHandler}/>
+      <p>quantity: {quantity}</p>
+
+      {/*  */}
+
+      <textarea onChange={} className="border" name="" placeholder="Enter delivery struction" id=""></textarea>
+
     </>
   );
 }
