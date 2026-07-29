42ceb18a26d527cce97a4ce4a337e3638b0c1fc7
Javohir <jafarjavohir2010@gmail.com>
2026-07-29 10:41:32 +0500

    commenting old works

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,86 @@
-import react, { useState } from "react";
+// import react, { useState } from "react";
 
-function App() {
+// function App() {
 
-  let [name, setName] = useState('Guest')
-  let [quantity, setQuantity] = useState(1)
-  let [comment, setComment] = useState('')
-  let [payment, setPayment] = useState('Visa')
-  let [delivery, setDelivery] = useState()
+//   let [name, setName] = useState('Guest')
+//   let [quantity, setQuantity] = useState(1)
+//   let [comment, setComment] = useState('')
+//   let [payment, setPayment] = useState('Visa')
+//   let [delivery, setDelivery] = useState('delivery')
 
-  function nameHandler(e) {
-    setName(e.target.value)
-  }
-  function quantityHandler(e) {
-    setQuantity(e.target.value)
-  }
-  function commentHandler(e) {
-    setComment(e.target.value)
-  }
-  function paymentHandler(e) {
-    setPayment(e.target.value)
-  }
-  function deliveryHandler(e) {
-    setDelivery(e.target.value)
-  }
-  return (
-    <>
-      <input onChange={nameHandler} className="border" type="text" name="" id="text" />
-      <p>Name: {name}</p>
-      <br />
-      {/*  */}
+//   function nameHandler(e) {
+//     setName(e.target.value)
+//   }
+//   function quantityHandler(e) {
+//     setQuantity(e.target.value)
+//   }
+//   function commentHandler(e) {
+//     setComment(e.target.value)
+//   }
+//   function paymentHandler(e) {
+//     setPayment(e.target.value)
+//   }
+//   function deliveryHandler(e) {
+//     setDelivery(e.target.value)
+//   }
+//   return (
+//     <>
+//       <input onChange={nameHandler} className="border" type="text" name="" id="text" />
+//       <p>Name: {name}</p>
+//       <br />
+//       {/*  */}
 
-      <input type="number" name="" id="number" className="border" onChange={quantityHandler} />
-      <p>quantity: {quantity}</p>
-      <br />
-      {/*  */}
-      <br />
-      <textarea onChange={commentHandler} className="border" name="" placeholder="Enter delivery struction" id=""></textarea>
-      <p>Comment: {comment}</p>
+//       <input type="number" name="" id="number" className="border" onChange={quantityHandler} />
+//       <p>quantity: {quantity}</p>
+//       <br />
+//       {/*  */}
+//       <br />
+//       <textarea onChange={commentHandler} className="border" name="" placeholder="Enter delivery struction" id=""></textarea>
+//       <p>Comment: {comment}</p>
 
-      {/*  */}
+//       {/*  */}
 
-      <select value={payment} onChange={paymentHandler} name="" id="">
-        <option value="visa">Visa</option>
-        <option value="mastercard">MasterCard</option>
-        <option value="paypal">PayPal</option>
-      </select>
-      <p>payment: {payment}</p>
+//       <select value={payment} onChange={paymentHandler} name="" id="">
+//         <option value="visa">Visa</option>
+//         <option value="mastercard">MasterCard</option>
+//         <option value="paypal">PayPal</option>
+//       </select>
+//       <p>payment: {payment}</p>
 
-      {/*  */}
+//       {/*  */}
 
-      <div className="radio" >
-        <input
-          type="radio"
-          name="delivery"
-          id="delivery"
-          value='delivery'
-          onChange={deliveryHandler}
-        />
+//       <div className="radio" >
+//         <input
+//           type="radio"
+//           name="delivery"
+//           id="delivery"
+//           value='delivery'
+//           checked={delivery === 'delivery'}
+//           onChange={deliveryHandler}
+          
+//         />
 
-        <label htmlFor="delivery">delivey</label>
+//         <label htmlFor="delivery">delivey</label>
 
-                  <br />
+//                   <br />
 
-        <input 
-          type="radio"
-          name="delivery" 
-          id="pickup" 
-          value='pickup'
-          onChange={deliveryHandler}
-        />
+//         <input 
+//           type="radio"
+//           name="delivery" 
+//           id="pickup" 
+//           value='pickup'
+//           onChange={deliveryHandler}
+//         />
 
-        <label htmlFor="pickup">pickup</label>
+//         <label htmlFor="pickup">pickup</label>
 
-      </div>
-      <p>Shipping: {delivery}</p>
+//       </div>
+//       <p>Shipping: {delivery}</p>
 
-    </>
-  );
-}
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
+
+ra
