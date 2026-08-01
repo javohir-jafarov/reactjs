580d665554015a387cd61c2b264e0d6ae7542801
Javohir <jafarjavohir2010@gmail.com>
2026-08-01 08:23:51 +0500

    project done

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
 
 const ColorPicker = () => {
+
+  let [color, setColor ] = useState('#ffffff')
+
+  let colorHandler = (e) => {
+    setColor(e.target.value)
+  }
+
   return (
-    <div>
-        <h1>Color Picker</h1>
-        <div className="colorBox">Selected color: <span>#ffffff</span></div>
-        <input type="color" name="" id="" />
+    <div className='flex flex-col items-center gap-4' >
+      <h1 className='title text-3xl inline '>Color Picker</h1>
+      <div className="colorBox w-64 h-64 rounded-2xl p-4 flex items-center justify-center border" style={{backgroundColor: color}}>
+
+        <p className=''>Selected color: <span>{color}</span></p>
+
+      </div>
+      <label htmlFor="picker">Pick the color 👇</label>
+      <input type="color" onChange={colorHandler} name="" id="picker" />
     </div>
   )
 }
