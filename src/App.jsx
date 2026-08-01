9dc056650b8fa14ea90af6f4d7a165514b635c88
Javohir <jafarjavohir2010@gmail.com>
2026-08-01 07:52:54 +0500

    start new project color picker

--- src/App.jsx
+++ src/App.jsx
@@ -83,4 +83,11 @@
 
 // export default App;
 
-ra
+import React from 'react'
+import ColorPicker from './components/ColorPicker'
+
+const App = () => {
+  return <ColorPicker />
+}
+
+export default App
---
+++ src/components/ColorPicker.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ColorPicker = () => {
+  return (
+    <div>
+        <h1>Color Picker</h1>
+        <div className="colorBox">Selected color: <span>#ffffff</span></div>
+        <input type="color" name="" id="" />
+    </div>
+  )
+}
+
+export default ColorPicker
