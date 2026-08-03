5d81d9391f2e89a49d5d6fb61230ee1cd5cc2084
Javohir <jafarjavohir2010@gmail.com>
2026-08-03 16:26:08 +0500

    practice started

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
 import React, {useState} from 'react'
 
-
-
 const App = () => {
 
-  let [car, setCar] = useState({year: 2024,
-                              brand: 'Ford', 
-                              model: 'mustang'})
-
-  let handleYearChange = (event) => {
-    setCar(prevCar => ({...prevCar, year: event.target.value}))
-  }
-
-  let handleBrandChange = (event) => {
-    setCar(prevCar => ({...prevCar, brand: event.target.value}))
-  }
-
-  let handleModelChange = (event) => {
-    setCar(prevCar =>({...prevCar, model: event.target.value}))
-  }
+  
 
   return (
     <div>
-      <p>Your favourite Car: {car.year} {car.brand} {car.model} </p>
 
-      <input type="number" value={car.year} onChange={handleYearChange}/>
-      <input type="text" value={car.brand} onChange={handleBrandChange}/>
-      <input type="text" value={car.model} onChange={handleModelChange}/>
+
+
     </div>
   )
 }
