bdd570cb73700418f6d1388adde56679ae83bedf
Javohir <jafarjavohir2010@gmail.com>
2026-08-03 13:42:31 +0500

    did something

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-import Counter from './components/Counter'
+import React, {useState} from 'react'
+
+let [car, setCar] = useState({year: 2024,
+                              brand: 'Ford', 
+                              model: 'mustang'})
 
 const App = () => {
-  return <Counter />
+  return (
+    <div>
+      <p>Your favourite Car: {car.year} {car.braand} {car.model} </p>
+
+      <input type="number" value={car.year}/>
+      <input type="text" value={car.brand}/>
+      <input type="text" value={car.model}/>
+    </div>
+  )
 }
 
 export default App
