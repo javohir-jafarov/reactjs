a6a4d47a5ab14d4e1b866bc0c55c28f6c61800b9
Javohir <jafarjavohir2010@gmail.com>
2026-08-04 15:17:42 +0500

    creating usestate and functions for it

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
 import React, {useState} from 'react'
 
 const App = () => {
+  let [foods, setFoods] = useState(['Apple', 'Banana', 'mango'])
 
-  let [car, setCar] = useState({
-    year: 2024,
-    brand: 'Ford',
-    model: 'Mustang'
-  })
+  let handleAddFood = () => {
 
-  let yearHandler = (e) => {
-    setCar(prevCar => ({...prevCar, year: e.target.value}))
   }
 
-  let brandHandler = (e) => {
-    setCar(prevCar => ({...prevCar, brand: e.target.value}))
+  let handleRemoveFood = () => {
+    
   }
-
-  let modelHandler = (e) => {
-    setCar(prevCar => ({...prevCar, model: e.target.value}))
-  }
-
   return (
     <div>
 
-      <p>Your favourite car: {car.year} {car.brand} {car.model}</p>
-        <input type="text" name="" id="" value={car.year} onChange={yearHandler}/>
-        <input type="text" name="" id="" value={car.brand} onChange={brandHandler}/>
-        <input type="text" name="" id="" value={car.model} onChange={modelHandler}/>
+      <h1>List of foods:</h1><br />
+      <ul>
+        {foods.map((food, index)=> <li>{food}</li>)}
+      </ul>
+
     </div>
   )
 }
