9d02ae4e009e5ddb5087430ddf94c729d6f9bc0b
Javohir <jafarjavohir2010@gmail.com>
2026-08-07 20:53:12 +0500

    create a blank App.jsx for repeat

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 const App = () => {
-  let [foods, setFoods] = useState(['Mango', 'Banana', 'Apple']);
-
-  let handleAddFood = () => {
-    let foodInput = document.getElementById('foodInput').value
-    document.getElementById('foodInput').value = ''
-    setFoods(f => ([...f, foodInput]))
-  }
-
-  let handleRemoveFood = (e) => {
-    setFoods(f => f.filter((_, i) => i !== e))
-  }
-
   return (
-    <div>
-      <h1>List of foods:</h1>
-      <br />
-      <ul>
-        {foods.map((food, index) => <li onClick={() => {handleRemoveFood(index)}} key={index}>{food}</li>)}
-      </ul>
-      <input type="text" id='foodInput'/> <button onClick={handleAddFood}>Add</button>
-    </div>
+    <div>App</div>
   )
 }
 
