7a346b148c4a029ce8eb9af4d175fe8010534ff3
Javohir <jafarjavohir2010@gmail.com>
2026-08-07 22:04:33 +0500

    practise done! :]

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,16 @@ const App = () => {
         document.getElementById('inputFood').value = ''
         setFoods(f => ([...f, inputFood]))
   }
+  let handleRemoveFood = (e) => {
+    setFoods(f => f.filter((_, i) => i !== e))
+  }
   return (
     <div>
 
       <h1>List of Foods:</h1>
 
       <ul>
-        {foods.map((food, index, arr) => <li key={index}>{food}</li>)}
+        {foods.map((food, index, arr) => <li onClick={() => {handleRemoveFood(index)}} key={index}>{food}</li>)}
       </ul>
 
       <input type="text" id='inputFood' placeholder='Enter food' />
