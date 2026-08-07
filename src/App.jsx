73c8ed8eadce040a5398b895a600b120abf0c305
Javohir <jafarjavohir2010@gmail.com>
2026-08-07 05:53:09 +0500

    layout created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>List of foods:</h1>
+      <ul></ul>
+      <input type="text" id='foodInput'/> <button>Add</button>
+    </div>
+  )
+}
+
+export default App
