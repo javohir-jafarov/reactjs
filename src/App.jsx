833c852c012afec73fc1eaebab43716475bd6ca9
Javohir <jafarjavohir2010@gmail.com>
2026-08-07 22:00:03 +0500

    handleAddFood created

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,24 @@ import React, {useState} from 'react'
 
 const App = () => {
   let [foods, setFoods] = useState(['Apple', 'Somsa', 'Taco'])
+
+  let handleAddFood = () => {
+        let inputFood = document.getElementById('inputFood').value
+        document.getElementById('inputFood').value = ''
+        setFoods(f => ([...f, inputFood]))
+  }
   return (
     <div>
 
-      <h1></h1>
+      <h1>List of Foods:</h1>
+
+      <ul>
+        {foods.map((food, index, arr) => <li key={index}>{food}</li>)}
+      </ul>
+
+      <input type="text" id='inputFood' placeholder='Enter food' />
+      <button onClick={handleAddFood}>Add</button>
+      
 
     </div>
   )
