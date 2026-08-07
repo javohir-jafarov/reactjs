b163cb8053a4ebd5f51b0b738a73889d0a2ef785
Javohir <jafarjavohir2010@gmail.com>
2026-08-07 06:08:35 +0500

    practice done i think :]

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import React, {useState} from 'react'
 
 const App = () => {
-  let [foods, setFoods] = useState(['Mango', 'Banana', 'Apple'])
+  let [foods, setFoods] = useState(['Mango', 'Banana', 'Apple']);
+
+  let handleAddFood = () => {
+    let foodInput = document.getElementById('foodInput').value
+    document.getElementById('foodInput').value = ''
+    setFoods(f => ([...f, foodInput]))
+  }
+
+  let handleRemoveFood = (e) => {
+    setFoods(f => f.filter((_, i) => i !== e))
+  }
+
   return (
     <div>
       <h1>List of foods:</h1>
+      <br />
       <ul>
-        
+        {foods.map((food, index) => <li onClick={() => {handleRemoveFood(index)}} key={index}>{food}</li>)}
       </ul>
-      <input type="text" id='foodInput'/> <button>Add</button>
+      <input type="text" id='foodInput'/> <button onClick={handleAddFood}>Add</button>
     </div>
   )
 }
