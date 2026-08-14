0eeb8c02f7e5856bf675e9c65e10a428091ca519
Javohir <jafarjavohir2010@gmail.com>
2026-08-14 16:47:52 +0500

    done with stucter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
+  let [cars, setCars] = useState([]);
+  let [year, setYear] = useState('');
+  let [brand, setBrand] = useState('');
+  let [model, setModel] = useState('');
+
+  let handleYear = () => {
+
+  }
+  let handleBrand = () => {
+    
+  }
+  let handleModel = () => {
+    
+  }
+  let handleRemover = () => {
+    
+  }
+  
   return (
-    <div>App</div>
+    <div>
+      List of cars:
+      <ul></ul>
+      <input type="number" id='year' placeholder='year'/>
+      <input type="text" id='brand' placeholder='brand'/>
+      <input type="text" id='model' placeholder='model'/>
+      <button>Add</button>
+    </div>
   )
 }
 
