7724098601326d0211e7b29dac52a28f16f873d7
Javohir <jafarjavohir2010@gmail.com>
2026-08-15 09:46:22 +0500

    i had some issues

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,42 @@ const App = () => {
   let [year, setYear] = useState('');
   let [brand, setBrand] = useState('');
   let [model, setModel] = useState('');
-
-  let handleYear = () => {
-
+  let handleAdd = () => {
+    let newCar = {
+      year: year,
+      brand: brand,
+      model: model
+    }
+    console.log(cars)
+    setCars(c => ([...c, newCar]))
+    console.log(cars)
   }
-  let handleBrand = () => {
-    
+
+  let handleYear = (event) => {
+    setYear(event.target.value)
   }
-  let handleModel = () => {
-    
+  let handleBrand = (event) => {
+    setBrand(event.target.value)
+  }
+  let handleModel = (event) => {
+    setModel(event.target.value)
   }
   let handleRemover = () => {
     
   }
+
+  console.log(year, brand, model)
   
   return (
     <div>
       List of cars:
-      <ul></ul>
-      <input type="number" id='year' placeholder='year'/>
-      <input type="text" id='brand' placeholder='brand'/>
-      <input type="text" id='model' placeholder='model'/>
-      <button>Add</button>
+      <ul>
+        {cars.map((car, index) => (<li key={index}>{car}</li>))}
+      </ul>
+      <input type="number" id='year' placeholder='year' onChange={(e) => {handleYear(e)}}/>
+      <input type="text" id='brand' placeholder='brand' onChange={(e) => {handleBrand(e)}}/>
+      <input type="text" id='model' placeholder='model' onChange={(e) => {handleModel(e)}}/>
+      <button onClick={handleAdd}>Add</button>
     </div>
   )
 }
