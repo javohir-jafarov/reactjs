bcd7c9aa08c14d8bb308e6b162101f64b896e90d
Javohir <jafarjavohir2010@gmail.com>
2026-09-04 21:44:07 +0500

    adding is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Card from './components/Card'
 const App = () => {
   let studentsData = [
-    {id: 0, name: 'Javohir', age: 16},
-    {id: 0, name: 'Anvar', age: 12},
+    { id: 0, name: 'Javohir', age: 16 },
+    { id: 1, name: 'Anvar', age: 12 },
   ]
   let [students, setStudents] = useState(studentsData)
+
+  let addHandler = () => {
+    let nameInp = document.getElementById('nameInp').value
+    document.getElementById('nameInp').value = ''
+
+    let ageInp = +document.getElementById('ageInp').value
+
+    if (nameInp.trim() != '' && ageInp !== 0) {
+      document.getElementById('nameInp').value = ''
+      document.getElementById('ageInp').value = ''
+      let id = students.at(-1).id
+      console.log(id)
+      let obj = {
+        id: id + 1,
+        name: nameInp,
+        age: ageInp
+      }
+      setStudents(s => [...s, obj])
+      console.log(students)
+    }
+
+    let handleDelete = () => {
+      students.filter()
+    }
+
+  }
   return (
     <div>
+      <div className="form">
+        <input type="text" placeholder='Name...' id='nameInp' />
+        <input type="number" name="" placeholder='Age...' id="ageInp" />
+        <button onClick={() => addHandler()}>Add</button>
+      </div>
       <div className="studnets">
-        {students.map((item, index) => (<Card name={item.name} key={index} age={item.age}/>))}
+        {students.map((item, index) => (<Card name={item.name} key={index} age={item.age} delete={() => {handleDelete}}/>))}
       </div>
     </div>
   )
-  
+
 }
 
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Card = (props) => {
   return (
-    <div key={props.key} className='flex flex-col min-w-20 border h-fit'>
-        <h1 className='name inline'>{props.name}</h1>
-        <p className='age inline'>{props.age}</p>
+    <div key={props.key} className='flex flex-col max-w-40 min-w-20 border h-fit'>
+        <h1 className='name '>{props.name}</h1>
+        <p className='age '>{props.age}</p>
+        <button className="delete bg-red-600 text-white cursor-pointer" onClick={props.delete}>Delete</button>
     </div>
   )
 }
