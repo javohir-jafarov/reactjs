66baa053325c9dffbf7f153b12fbbbdca592d33c
Javohir <jafarjavohir2010@gmail.com>
2026-09-04 19:40:43 +0500

    clean up space for students list practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 const App = () => {
-  let [count, setCount] = useState(0)
   return (
-    <div>
-      <h1 className='text-5xl'>{count}</h1>
-        <div className='btns flex gap-4'>
-          <button onClick={() => {setCount(c => c + 1)}}>+</button>
-          <button onClick={() => {count !== 0 && setCount(c => c - 1)}}>-</button>
-          <button onClick={() => setCount(0)}>reset</button>
-        </div>
-    </div>
+    <div>App</div>
   )
 }
 
